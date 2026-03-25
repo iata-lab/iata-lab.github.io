a43858d92c1189014df32b54227e4735eb5f14ca
Pc-casa <inigobasquetrade@outlook.es>
2026-03-25 21:04:14 +0100

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.js
 export default defineConfig({
   plugins: [react()],
   base: "/",
@@ -14,7 +10,9 @@ export default defineConfig({
     outDir: "dist",
     rollupOptions: {
       output: {
-        assetFileNames: "assets/[name][extname]",
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
       },
     },
   },
