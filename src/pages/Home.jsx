4ffae56ad29050be21e09c9b43e16ff17dfab2f1
Iata <bootcepum@gmail.com>
2026-04-25 10:18:35 +0200

    Fix spelling and enhance clarity in Home.jsx
    Corrected spelling errors and improved sentence structure for clarity.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
   <p>
     He trabajado en el Programa de las Naciones Unidas en Chile, en
     Banca de Inversión y también en la industria automovilística, donde
-    desarolle una mentalidad analítica y una visión estratégica
+    desarrolle una mentalidad analítica y una visión estratégica
     transversal que marcan mi forma de entender el entorno empresarial.
   </p>
   <p>
@@ -30,21 +30,22 @@ const Home = () => {
     priorizar con claridad y conectar con la realidad de muchas personas.
   </p>
   <p>
-    En 2022 aposté por presentarme a una plaza fija en el Instituto Nacional
-    de Estadística (INE), dedicando dos años y medio a preparar oposiciones
-    de forma constante. Superé el proceso en dos ocasiones, aunque sin
-    alcanzar la nota de corte. Este proceso supuso un punto de inflexión
-    personal: ante la falta de bolsa de empleo o interinidad en el Ministerio
-    de Economía, no tenía posibilidades financieras para seguir estudiando y
-    busqué en mí lo que siempre se me ha dado mejor: crecer haciendo, más que
-    desde lo académico, y apoyarme en la interacción humana.
+    En 2022 aposté por presentarme a una plaza fija en el Instituto
+    Nacional de Estadística (INE), dedicando dos años y medio a preparar
+    oposiciones de forma constante. Superé el concurso en dos ocasiones,
+    aunque sin alcanzar la nota de corte. Este proceso ha supuesto un punto
+    de inflexión personal: ante la falta de bolsa de empleo o interinidad,
+    no tenía posibilidades financieras para seguir, mientras me daba cuenta
+    que buscaba en mí lo que siempre se me ha dado mejor: crecer haciendo,
+    más que desde lo académico, y apoyarme en la interacción con las
+    personas.
   </p>
   <p>
-    Así, compaginé mis estudios con experiencia laboral en Europa,
-    principalmente en una empresa de distribución de productos químicos y
-    otros trabajos temporales, como en una fábrica en Noruega, mientras
-    consolido mis conocimientos en inteligencia artificial y lenguajes de
-    programación.
+    Así, estos últimos 2 años he compaginado mis estudios con experiencia
+    laboral en Europa, principalmente en una empresa de distribución de
+    productos químicos y otros trabajos temporales, como en una fábrica en
+    Noruega, mientras consolido mis conocimientos en inteligencia
+    artificial y en lenguajes de programación.
   </p>
 </div>
       </article>
